27f4906bd67bcba1c0e35af1a23e86ab3442d081
Emer <emir05051@gmail.com>
2022-06-20 16:44:03 +0200

    update 1

--- app/src/main/java/com/fitnessproject/myapplication/LoginActivity.java
+++ app/src/main/java/com/fitnessproject/myapplication/LoginActivity.java
@@ -2,16 +2,30 @@ package com.fitnessproject.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.fitnessproject.myapplication.R;
+import com.fitnessproject.myapplication.databinding.ActivityLoginBinding;
+import com.fitnessproject.myapplication.databinding.ActivitySelectionBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.login.setOnClickListener(v -> {
+            goToMain();
+        });
+    }
+    private void goToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
